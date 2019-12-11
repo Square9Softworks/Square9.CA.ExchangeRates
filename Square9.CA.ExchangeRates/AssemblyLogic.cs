78834d61b2f124f63fe60927b6353e9842a2ed9c
Brian Banet <bbanet@square-9.com>
2019-12-11 16:42:21 -0500

    Add project files.

---
+++ Square9.CA.ExchangeRates/AssemblyLogic.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Square9.CA.ExchangeRates
+{
+    public class AssemblyLogic
+    {
+        public Dictionary<string, string> RunCallAssembly(Dictionary<string, string> Input)
+        {
+            Dictionary<string, string> Output = new Dictionary<string, string>();
+            String url = "https://api.ratesapi.io/api/latest?base=USD";
+
+            String amount = "";
+            String currency = "";
+            String returnKey = "";
+
+            foreach(String item in Input.Keys)
+            {
+                if (!String.IsNullOrEmpty(Input[item]) && Input[item].ToUpper().StartsWith("AMOUNT:"))
+                    amount = Input[item].Substring(7);
+
+                if (!String.IsNullOrEmpty(Input[item]) && Input[item].ToUpper().StartsWith("CURRENCY:"))
+                    currency = Input[item].Substring(9);
+
+                if (!String.IsNullOrEmpty(Input[item]) && Input[item].ToUpper().StartsWith("CONVERTED:"))
+                    returnKey = item;
+            }
+
+            if (amount == "" || currency == "" || returnKey == "")
+            {
+                Console.WriteLine(DateTime.Now.ToString() + "\tMissing required parameters.");
+                return Output;
+            }
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    String jsonStr = reader.ReadToEnd();
+                    dynamic exchange = JsonConvert.DeserializeObject(jsonStr);
+                    double currentRate = exchange.rates[currency].Value;
+                    Console.WriteLine(DateTime.Now.ToString() + "\tCurrent rate for " + currency + " is " + currentRate.ToString());
+                    double converted = currentRate * Convert.ToDouble(amount);
+                    Console.WriteLine(DateTime.Now.ToString() + "\tConverted amount is " + Math.Round(converted, 2).ToString());
+
+                    Output.Add(returnKey, Math.Round(converted, 2).ToString());
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(DateTime.Now.ToString() + "\t" + ex.Message);
+            }
+
+            return Output;
+        }
+    }
+}
---
+++ Square9.CA.ExchangeRatesTestConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square9.CA.ExchangeRatesTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExchangeRates.AssemblyLogic logic = new ExchangeRates.AssemblyLogic();
+            Dictionary<string, string> testData = new Dictionary<string, string>();
+            testData.Add("-2", "");
+            testData.Add("1", "Amount:65329.28");
+            testData.Add("3", "Currency:GBP");
+            testData.Add("4", "Converted:");       
+            testData.Add("5", "");
+            testData.Add("7", null);
+
+            Console.WriteLine("Call Assembly Test - Rate Conversion");
+
+            Dictionary<string,string> processed = logic.RunCallAssembly(testData);
+            
+            foreach(String item in processed.Keys)
+            {
+                Console.WriteLine(item + " - " + processed[item]);
+            }
+
+            Console.Read();
+        }
+    }
+}
